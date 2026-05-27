369939d518114b27563f0473952fb17838931cf1
HuacongXie <solarsys@outlook.com>
2026-05-26 21:09:19 -0400

    add minimal Spring Boot API app

---
+++ backend/perkpilot-api-app/src/main/java/com/perkpilot/api/PerkPilotApiApplication.java
@@ -0,0 +1,12 @@
+package com.perkpilot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerkPilotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerkPilotApiApplication.class, args);
+    }
+}
