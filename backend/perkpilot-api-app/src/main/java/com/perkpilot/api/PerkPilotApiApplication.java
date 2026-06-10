641d439a64ed2c8cce4622b6256382bb41467cf0
HuacongXie <solarsys@outlook.com>
2026-06-10 14:34:12 -0400

    added MapperScan annotation

--- backend/perkpilot-api-app/src/main/java/com/perkpilot/api/PerkPilotApiApplication.java
+++ backend/perkpilot-api-app/src/main/java/com/perkpilot/api/PerkPilotApiApplication.java
@@ -1,9 +1,10 @@
 package com.perkpilot.api;
-
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.perkpilot.api")
 public class PerkPilotApiApplication {
 
     public static void main(String[] args) {
